801f823c011210a3feab67209456bd11d56077b3
eloylozano <lozanobarrioseloy@gmail.com>
2025-01-09 09:04:42 +0100

    Swagger configuration done

--- Backend/dataBase/Deskify/src/main/java/com/deskify/configuration/AppConfig.java
+++ Backend/dataBase/Deskify/src/main/java/com/deskify/configuration/AppConfig.java
@@ -5,14 +5,27 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 
 @Configuration
 public class AppConfig {
 
-    // ModelMapper
+    // ModelMapperConfig
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    // SwaggerConfig
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Deskify API")
+                        .version("1.0.0")
+                        .description("API documentation for Deskify"));
     }
 
 }
