cdec6f364677616af9dd9a5102e15118231e19ed
eloylozano <lozanobarrioseloy@gmail.com>
2025-01-22 13:37:11 +0100

    documentation

--- Backend/Deskify/src/main/java/com/deskify/controller/CommentController.java
+++ Backend/Deskify/src/main/java/com/deskify/controller/CommentController.java
@@ -10,11 +10,7 @@ import com.deskify.dto.CommentResponseDTO;
 import com.deskify.dto.CommentTextRequestDTO;
 import com.deskify.service.CommentService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ticket")
