0a38fd8459b8648e7397210bf6048344d00b8b52
eloylozano <lozanobarrioseloy@gmail.com>
2025-03-22 15:27:04 +0100

    feat index

---
+++ FrontEnd/deskify/tailwind.config.mjs
@@ -0,0 +1,14 @@
+// tailwind.config.mjs
+export default {
+    theme: {
+        extend: {
+            colors: {
+                'background': '#02353C',
+                'primary': '#01C883',
+                'secondary': '#00F977',
+                'accent': '#00CFF7',
+            },
+        },
+    },
+    plugins: [],
+}
